6cd525f71c2a2ac4ed7a71d42f80d474ab3c054a
Edilberto Contreras <edilberto.contreras@upb.edu.co>
2026-01-23 15:10:41 -0500

    Se creo el codigo de numeros primos

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,2 +1,19 @@
 #Mi primer scrip .py
 print("hola mundo")
+
+n = 1000
+es_primo = [True] * (n + 1)
+
+es_primo[0] = False
+es_primo[1] = False
+
+for i in range(2, int(n ** 0.5) + 1):
+    if es_primo[i]:
+        for j in range(i * i, n + 1, i):
+            es_primo[j] = False
+
+print("Numero primo entre 1 y 1000:")
+for i in range(2, n + 1):
+    if es_primo[i]:
+        print(i, end=" ")
+
